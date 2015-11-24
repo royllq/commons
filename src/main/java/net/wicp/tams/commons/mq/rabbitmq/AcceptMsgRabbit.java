fcbb72d30a7dfe444fdafb66545888a5bcc44847
andy.zhou <rjzjh@163.com>
2015-11-25 07:10:07 +0800

    1

---
+++ src/main/java/net/wicp/tams/commons/mq/rabbitmq/AcceptMsgRabbit.java
@@ -0,0 +1,103 @@
+package net.wicp.tams.commons.mq.rabbitmq;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
+
+import org.slf4j.Logger;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
+
+import net.wicp.tams.commons.LogHelp;
+import net.wicp.tams.commons.thread.ThreadPool;
+
+/***
+ * RabbitMQ的接收者
+ * 
+ * @author andy.zhou
+ *
+ */
+public class AcceptMsgRabbit extends Observable {
+	private final Logger logger = LogHelp.getLogger(getClass());
+	public static final String COMMONS = "NOTAG";// 默认标签
+	private static Map<String, List<Observer>> obsmap = new HashMap<>();
+
+	private static final AcceptMsgRabbit INSTANCE = new AcceptMsgRabbit();
+
+	public static AcceptMsgRabbit getInstance() {
+		return INSTANCE;
+	}
+
+	@Override
+	public synchronized void addObserver(Observer observer) {
+		addObserver(COMMONS, observer);
+	}
+
+	public synchronized void addObserver(String consumerSource, Observer observer) {
+		boolean needConsumer = false;// 是否需要构建新消费者
+		if (obsmap.get(consumerSource) == null) {
+			obsmap.put(consumerSource, new ArrayList<Observer>());
+			needConsumer = true;
+		}
+		addObserver(consumerSource, observer, needConsumer);
+	}
+
+	private void messageArrived(String tag, Object message) {
+		for (Observer observer : obsmap.get(tag)) {
+			observer.update(this, message);
+		}
+	}
+
+	public synchronized void addObserver(final String consumerSource, Observer observer, boolean needConsumer) {
+		obsmap.get(consumerSource).add(observer);
+		if (needConsumer) {
+			ThreadPool.getDefaultPool().submit(new Runnable() {
+
+				@Override
+				public void run() {
+					Channel channel = null;// TODO 得到通道
+					try {
+						QueueingConsumer consumer = new QueueingConsumer(channel);
+						// 默认是需要consumer收到消息后才进行ack
+						boolean autoAck = false;
+						channel.basicConsume(consumerSource, autoAck, consumer);// TODO
+																				// 怎么把consumerSource与Query名对应上
+						while (true) {
+							QueueingConsumer.Delivery delivery = consumer.nextDelivery(3000);
+							if (delivery == null) {
+								continue;
+							}
+							byte[] message = delivery.getBody();
+
+							try {
+								messageArrived(consumerSource, new String(message));
+								channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+							} catch (Exception e) {
+								logger.error("接收消息时失败", e);
+							}
+
+						}
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (ShutdownSignalException e) {
+						// AutoRetryConnectionFactory.getInstance().init();
+						e.printStackTrace();
+					} catch (ConsumerCancelledException e) {
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						// AutoRetryConnectionFactory.getInstance().closeChannelAndConnection();
+					}
+				}
+			});
+		}
+	}
+
+}
--- src/main/java/net/wicp/tams/commons/thread/ThreadPool.java
+++ src/main/java/net/wicp/tams/commons/thread/ThreadPool.java
@@ -7,11 +7,27 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import javax.security.auth.callback.Callback;
+
 import org.apache.commons.lang3.StringUtils;
+
+import net.wicp.tams.commons.Conf;
 
 public class ThreadPool {
 	private static final java.util.Map<String, ExecutorService> executorServiceMap = new java.util.HashMap<String, ExecutorService>();
-
+	private static Properties newProperties=null;
+	static{
+		Conf.addCallBack("ThreadPool", new Conf.Callback() {		
+			@Override
+			public void doReshConf(Properties newProperties) {
+				ThreadPool.newProperties=newProperties;
+				for (String poolname : executorServiceMap.keySet()) {
+					executorServiceMap.get(poolname).shutdown();
+				}
+				executorServiceMap.clear();
+			}
+		}, "thread.pool.%s");
+	}
 	/*****
 	 * 通过名字得到线程池
 	 * 
@@ -64,8 +80,8 @@ public class ThreadPool {
 	 * 
 	 * @return
 	 */
-	public static final ExecutorService getDefaultPool(Properties properties) {
-		return getThreadPoolByName("default", properties);
+	public static final ExecutorService getDefaultPool() {
+		return getThreadPoolByName("default", ThreadPool.newProperties);
 	}
 
 	private static TimeUnit getTimeUnit(String TimeUnitStr) {
