f1c5991120db313a59c88cef484cbaaf6a4dd8ae
andy.zhou <rjzjh@163.com>
2015-11-29 09:09:38 +0800

    1

--- src/test/java/net/wicp/tams/commons/test/AbsToDynaBean.java
+++ src/test/java/net/wicp/tams/commons/test/AbsToDynaBean.java
@@ -5,7 +5,7 @@ import net.wicp.tams.commons.apiext.IOUtil;
 import org.apache.log4j.PropertyConfigurator;
 
 public class AbsToDynaBean {
-	protected static final String dir = "D:/workspace/commons/src/test/resources/connector";
+	protected static final String dir = "D:/source/github/commons/src/test/resources/connector";
 	static {
 		PropertyConfigurator.configure(IOUtil.class
 				.getResourceAsStream("/log_util.properties"));
