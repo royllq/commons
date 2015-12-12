1c0724d2f892c95c1e138f7b610cdf449cc9a801
andy.zhou <rjzjh@163.com>
2015-12-12 22:04:52 +0800

    1

--- src/main/java/net/wicp/tams/commons/report/excel/ReportAbstract.java
+++ src/main/java/net/wicp/tams/commons/report/excel/ReportAbstract.java
@@ -1,8 +1,16 @@
 package net.wicp.tams.commons.report.excel;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.apache.log4j.Logger;
 
 import net.wicp.tams.commons.Conf;
+import net.wicp.tams.commons.apiext.IOUtil;
+import net.wicp.tams.commons.apiext.StringUtil;
 import net.wicp.tams.commons.constant.PathType;
 
 public abstract class ReportAbstract {
@@ -12,7 +20,27 @@ public abstract class ReportAbstract {
 
 	protected final String tempName;
 
-	public abstract void export(boolean issave);
+	public String exportExcel(boolean isSaveFile) {
+		InputStream is = null;
+		try {
+			is = StringUtil.isNull(tempName) ? null
+					: new FileInputStream(IOUtil.mergeFolderAndFilePath(tempdir, tempName));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("模板文件不存在");
+		}
+		OutputStream os = null;
+		export(is, os);
+		if (is != null) {
+			try {
+				is.close();
+			} catch (IOException e) {
+				logger.info("关闭输入流出错");
+			}
+		}
+		return "";
+	}
+
+	public abstract void export(InputStream is, OutputStream os);
 
 	/****
 	 * 没有模板文件直接导出文件
