ce53e5d3af58fd5e626b80d771c73a283cea1fdb
zhoujunhui <zhoujunhui@tsjinrong.cn>
2015-12-26 16:53:52 +0800

    1

--- src/main/java/net/wicp/tams/commons/web/EasyUiAssist.java
+++ src/main/java/net/wicp/tams/commons/web/EasyUiAssist.java
@@ -177,40 +177,7 @@ public abstract class EasyUiAssist {
 		return getJsonForGridAlias(retList, retList.size());
 	}
 
-	/**
-	 * 把List转为Jquery select的model
-	 * 
-	 * @param fromList
-	 *            要转为json的List
-	 * @param nameFiled
-	 *            label的字段名
-	 * @param codeFiled
-	 *            code的字段名
-	 * @param idName
-	 *            属性字段名
-	 * @return String
-	 *         [{label:'男',value:'M'},{label:'女',value:'F'},{label:'未知',value:''
-	 *         } ]
-	 * @throws ProjectException
-	 */
-	public static String getJsonFromList(List<Object> fromList, String nameFiled, String codeFiled, String idName)
-			throws ProjectException {
-		if (CollectionUtils.isEmpty(fromList) || StringUtil.isNull(nameFiled) || StringUtil.isNull(codeFiled)) {
-			throw new ProjectException(ExceptAll.param_error, "要解析的参数错误");
-		}
-		if (StringUtil.isNull(idName)) {
-			idName = codeFiled;// 如果缺少ID则把code字段做为ＩＤ
-		}
-		String jsonTempStr = "@['{label:\"'+" + nameFiled + "+'\",value:\"'+" + codeFiled + "+'\",id:\"'+" + idName
-				+ "+'\"},']";
-		StringBuffer returnBuff = new StringBuffer();
-		for (Object object : fromList) {
-			returnBuff.append(TemplateRuntime.eval(jsonTempStr, object));
-		}
-		returnBuff.delete(returnBuff.length() - 1, returnBuff.length());// 去除最后一个“,”
-		// returnBuff.append("]}");
-		return returnBuff.toString();
-	}
+	
 
 	/****
 	 * 把根节点转为json Str字符串
